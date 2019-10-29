7759f2ede9a8ff32830d838b1da5ec9a46968e65
YikesHome <31678324+yikeshome@users.noreply.github.com>
2019-10-30 03:48:41 +0800

    Update profile.go

--- profiling/profile.go
+++ profiling/profile.go
@@ -10,3 +10,16 @@ func Duration(invocation time.Time, name string) {
 
     log.Printf("%s lasted %s", name, elapsed)
 }
+
+func BigIntFactorial(x big.Int) *big.Int {
+    // Arguments to a defer statement is immediately evaluated and stored.
+    // The deferred function receives the pre-evaluated values when its invoked.
+    defer Duration(time.Now(), "IntFactorial")
+
+    y := big.NewInt(1)
+    for one := big.NewInt(1); x.Sign() > 0; x.Sub(x, one) {
+        y.Mul(y, x)
+    }
+
+    return x.Set(y)
+}
