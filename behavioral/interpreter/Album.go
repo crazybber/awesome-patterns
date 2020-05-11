8ac63fef970e2deb9297f99737d4927cfdb779d4
Edward <chenlulei@gmail.com>
2020-05-11 21:17:16 +0800

    Create Album.go

---
+++ behavioral/interpreter/Album.go
@@ -0,0 +1,12 @@
+/**
+ * @author  : Jagepard <jagepard@yandex.ru>
+ * @license https://mit-license.org/ MIT
+ */
+
+package main
+
+// Album is ...
+type Album struct {
+	name   string
+	author string
+}
