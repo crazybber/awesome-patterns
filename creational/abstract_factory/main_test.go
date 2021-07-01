c7042966dce8d0eaa478539e92e5f76ad5d0468c
Lucas Alves <luk4z_7@hotmail.com>
2021-07-01 01:00:56 -0300

    Created a file that has an eg usage of factory

---
+++ creational/abstract_factory/main_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"testing"
+)
+
+var c = CarFactory{}
+
+func TestCarFactory(t *testing.T) {
+
+	carFactory, err := CreateVehicleFactory(CAR)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	luxuryCar, err := carFactory.NewVehicle(LuxuryCarType)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	car, ok := luxuryCar.(Car)
+	if !ok {
+		t.Fatal("struct assertion failed")
+	}
+
+	t.Logf("CarType: Luxury NumWheels=%d, NumSeats=%d, NumDoors=%d",
+		luxuryCar.NumWheels(), luxuryCar.NumSeats(), car.NumDoors())
+}
